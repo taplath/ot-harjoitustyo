07f70d0266ff61ee763a37c6b977947daf27e04f
Tapani Plathan <tapani.platha@gmail.com>
2021-03-20 23:40:14 +0200

    Project template added

---
+++ COVIDHack21/src/main/java/com/tplathan/covidhack21/Main.java
@@ -0,0 +1,23 @@
+package com.tplathan.covidhack21;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Hello, JavaFX.");
+        Scene scene = new Scene(new StackPane(label), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ COVIDHack21/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.tplathan.covidhack21 {
+    requires javafx.controls;
+    exports com.tplathan.covidhack21;
+}
