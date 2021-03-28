254704a78b6a5907fecc861b38d813287e588aaa
Tapani Plathan <tapani.platha@gmail.com>
2021-03-28 12:34:32 +0300

    Removed bad code

--- COVIDHack21/src/main/java/com/tplathan/covidhack21/gui/Gui.java
+++ COVIDHack21/src/main/java/com/tplathan/covidhack21/gui/Gui.java
@@ -7,15 +7,11 @@ import javafx.scene.layout.GridPane;
 public class Gui {
 
     public Scene createScene() {
-        // TODO: BorderPane centering troubleshoot and sizing
-        // BorderPane bp = new BorderPane();
-
         DrawBoard draw = new DrawBoard();
         GridPane gp = draw.drawBoard(StaticLevel.LEVEL);
-
-        //  bp.getChildren().add(gp);
-        // bp.setCenter(gp);
+        
         Scene scene = new Scene(gp, 640, 480);
+        
         return scene;
 
     }
