db39d2dec44cb56098e7fff3c4ffcf6a48ccbb56
Tapani Plathan <tapani.platha@gmail.com>
2021-04-24 15:22:12 +0300

    Fixed LevelLoader tests

--- COVIDHack21/src/test/java/com/tplathan/covidhack21/gamelogic/LevelLoaderTest.java
+++ COVIDHack21/src/test/java/com/tplathan/covidhack21/gamelogic/LevelLoaderTest.java
@@ -1,16 +1,8 @@
 package com.tplathan.covidhack21.gamelogic;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-/**
- *
- * @author plathant
- */
 public class LevelLoaderTest {
 
     @Test
