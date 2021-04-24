529675ad90a5ce42e8b1f8808ff7ea8d228f228a
Tapani Plathan <tapani.platha@gmail.com>
2021-04-24 15:25:48 +0300

    Added whitespaces to satisfy checkstyle needs

--- COVIDHack21/src/main/java/com/tplathan/covidhack21/gamelogic/Direction.java
+++ COVIDHack21/src/main/java/com/tplathan/covidhack21/gamelogic/Direction.java
@@ -2,10 +2,10 @@ package com.tplathan.covidhack21.gamelogic;
 
 public enum Direction {
 
-    UP(0,-1),
-    DOWN(0,1),
-    LEFT(-1,0),
-    RIGHT(1,0);
+    UP(0, -1),
+    DOWN(0, 1),
+    LEFT(-1, 0),
+    RIGHT(1, 0);
 
     public final int x;
     public final int y;
