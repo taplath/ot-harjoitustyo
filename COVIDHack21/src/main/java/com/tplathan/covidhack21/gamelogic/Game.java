c105ecb3212d01e035e91e6bcd15e83a701b0f1d
Tapani Plathan <tapani.platha@gmail.com>
2021-04-24 13:35:18 +0300

    Added logic for going to next level

--- COVIDHack21/src/main/java/com/tplathan/covidhack21/gamelogic/Game.java
+++ COVIDHack21/src/main/java/com/tplathan/covidhack21/gamelogic/Game.java
@@ -1,40 +1,51 @@
 package com.tplathan.covidhack21.gamelogic;
 
+import java.util.ArrayList;
+
 public class Game {
+
     private boolean isWon;
     private boolean isLost;
-    private Level currentLevel;
+    private ArrayList<Level> levels;
+    private int currentLevelIndex;
     private Player player;
-    
+
     public Game() {
         this.isWon = false;
         this.isLost = false;
         this.player = new Player();
-        this.currentLevel = LevelLoader.firstLevel();
+        this.levels = new ArrayList<>();
+        this.levels.add(LevelLoader.firstLevel());
+        this.levels.add(LevelLoader.secondLevel());
+
     }
-    
+
     public boolean isWon() {
         return isWon;
     }
-    
+
     public boolean isLost() {
         return isLost;
     }
-    
+
     public void movePlayer(Direction direction) {
-        this.currentLevel.movePlayer(direction);
-        
-        if (this.currentLevel.isLost()) {
+        this.getCurrentLevel().movePlayer(direction);
+
+        if (this.getCurrentLevel().isLost()) {
             this.isLost = true;
             return;
         }
-        
-        if(currentLevel.isWon()) {
-            this.isWon = true;
+
+        if (getCurrentLevel().isWon()) {
+            if (this.currentLevelIndex == this.levels.size() - 1) {
+                this.isWon = true;
+            } else {
+                this.currentLevelIndex++;
+            }
         }
     }
-    
+
     public Level getCurrentLevel() {
-        return this.currentLevel;
+        return this.levels.get(this.currentLevelIndex);
     }
 }
--- COVIDHack21/src/main/java/com/tplathan/covidhack21/gamelogic/LevelLoader.java
+++ COVIDHack21/src/main/java/com/tplathan/covidhack21/gamelogic/LevelLoader.java
@@ -62,5 +62,29 @@ public class LevelLoader {
         return level;
     }
     
+    public static Level secondLevel() {
+        Level level = new Level("Level 2");
+        
+        HashMap<Coordinate, TerrainType> terrainMap = new HashMap<>();
+        for (int y = 0; y < LEVEL_BASE.length; y++) {
+            for (int x = 0; x < LEVEL_BASE[0].length; x++) {
+                char terrainMarker = LEVEL_BASE[y][x];
+                boolean isWall = false;
+                if (terrainMarker == '#') {
+                    isWall = true;
+                }
+                TerrainType terrain = new TerrainType(terrainMarker, isWall);
+                
+                // y coordinate 0 should be bottom, not top
+                int invertedY = LEVEL_BASE.length - y;
+                Coordinate coordinate = new Coordinate(x, invertedY);
+                terrainMap.put(coordinate, terrain);
+            }
+        }
+        
+        level.setTerrain(terrainMap);
+        return level;
+    }
+    
     
 }
