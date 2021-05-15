0937ca4a64b1b2c06269b0d1a8cadb9d5a018645
Tapani Plathan <tapani.platha@gmail.com>
2021-05-15 17:58:10 +0300

    Added documentation

--- COVIDHack21/src/main/java/com/tplathan/covidhack21/gamelogic/RandomDirection.java
+++ COVIDHack21/src/main/java/com/tplathan/covidhack21/gamelogic/RandomDirection.java
@@ -5,16 +5,17 @@ import static com.tplathan.covidhack21.gamelogic.Direction.LEFT;
 import static com.tplathan.covidhack21.gamelogic.Direction.RIGHT;
 import static com.tplathan.covidhack21.gamelogic.Direction.UP;
 
+/**
+ * Used to create a random direction for monsters to move in to
+ *
+ * @author Tapani Plathan
+ */
 public class RandomDirection {
 
-    private static int rollRandomNumber() {
-        int min = 1;
-        int max = 4;
-        int range = max - min + 1;
-        int number = (int) (Math.random() * range + min);
-        return number;
-    }
-
+    /**
+     *
+     * @return a random direction
+     */
     public Direction getRandomDirection() {
         int randomNumber = rollRandomNumber();
         if (randomNumber == 1) {
@@ -29,4 +30,15 @@ public class RandomDirection {
         return LEFT;
     }
 
+    /**
+     *
+     * @return a number between 1 and 4
+     */
+    private static int rollRandomNumber() {
+        int min = 1;
+        int max = 4;
+        int range = max - min + 1;
+        int number = (int) (Math.random() * range + min);
+        return number;
+    }
 }
